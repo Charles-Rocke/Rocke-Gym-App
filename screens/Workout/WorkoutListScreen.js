ae5103835187a0b5980ca1fc4a905e94869b49e6
Spook <spook.devv@gmail.com>
2024-05-03 20:35:42 -0400

    Added Workout and Workout List Screens

---
+++ screens/Workout/WorkoutListScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+function WorkoutListScreen() {
+  return (
+    <View>
+      <Text>Workout List Screen</Text>
+    </View>
+  );
+}
+
+export default WorkoutListScreen;
---
+++ screens/Workout/WorkoutScreen.js
@@ -0,0 +1,11 @@
+import { View } from "react-native";
+
+function WorkoutScreen() {
+  return (
+    <View>
+      <Text>Workout Screen</Text>
+    </View>
+  );
+}
+
+export default WorkoutScreen;
